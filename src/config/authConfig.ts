1ffafaa6b3a4d41b6c57f727b935acc9c9f7eb26
Simona Georgieva <simona.georgieva@cleverpine.com>
2024-09-04 14:15:23 +0300

    clean up auth config

--- src/config/authConfig.ts
+++ src/config/authConfig.ts
@@ -2,6 +2,7 @@ export const msalConfig = {
   auth: {
     clientId: 'bbaf726d-af5d-4d0c-b539-908dbe6125f1',
     authority: 'https://login.microsoftonline.com/2c4c412c-1cb6-4770-9bb4-87b4bfe440c1/oauth2/v2.0/authorize',
+    // redirectUri: 'https://domovoy2.thepineslab.net/
     redirectUri: 'http://localhost:3000/',
   },
   cache: {
@@ -10,11 +11,3 @@ export const msalConfig = {
   },
 };
 
-export const loginRequest = {
-  scopes: ['user.read', 'calendars.read'],
-};
-
-export const graphConfig = {
-  graphCalendarEndpoint: 'https://graph.microsoft.com/v1.0/me/calendar/getSchedule',
-  graphScheduleMeetingEndpoint: 'https://graph.microsoft.com/v1.0/me/events'
-};
